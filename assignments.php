30e03421e5d993a956321edfefcd79657dc811bd
Igor Prylepskyi <pry12002@byui.edu>
2015-04-30 00:25:32 -0600

    change

--- assignments.php
+++ assignments.php
@@ -4,20 +4,25 @@
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="style.css" 
               media="screen">
-    <title>Intro</title>
+    <title>Assignments</title>
 </head>
     <body>
         <header id="page_header">
-            <h1>Introduction</h1>
+            <h1>Web Engineering II Assignments</h1>
         </header>
         
         <main>
         	<img id="mypic" src="my-picture.jpg">
-        	<p>My name is Igor. I'm from Ukraine. I'm Senior in Software Engineering.</p>
+        	<table>
+                <ul>
+                    <li>Assignment 1</li>
+                    <li>Assignment 2</li>
+                </ul>
+            </table>
         </main>
 
         <footer id="page_footer">
-            <a href="assignments.php">assignments</a>
+            <a href="index.php">introduction</a>
         </footer>
     </body>
 </html>
