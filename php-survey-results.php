01260e0313dd5f2e772ff536aa1fd1eb4f5c5aaf
Igor Prylepskyi <pry12002@byui.edu>
2015-05-09 17:35:46 -0600

    commit

--- php-survey-results.php
+++ php-survey-results.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // Open the file to get existing content
     $content = file_get_contents('voting-results.txt');
-    echo (unserialize($content))[2];
+    echo (string)unserialize($content);
     
 } else {
     header('Location: php-survey.php');
