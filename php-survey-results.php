a94ae115fd77960ce973cbcd318e14b6823f6d27
Igor Prylepskyi <pry12002@byui.edu>
2015-05-08 22:48:50 -0600

    commit

--- php-survey-results.php
+++ php-survey-results.php
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Open the file to get existing content
     $current = file_get_contents($file);
     // Append a new person to the file
-    $current = "$prevAnswer = ". $answer. "\n";
+    $current = '$prevAnswer = '. $answer. "\n";
     // Write the contents back to the file
     file_put_contents($file, $current);
 
