7b128d21c2ea037112d2b0392858f00e1b1f8143
Igor Prylepskyi <pry12002@byui.edu>
2015-05-08 22:52:08 -0600

    commit

--- php-survey-results.php
+++ php-survey-results.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $current = '$prevAnswer = 0';
     eval($current);
     // Append a new person to the file
-    $current = '$prevAnswer = '. "$prevAnswer + 1\n";
+    $current = '$prevAnswer = '. $prevAnswer + 1. "\n";
     // Write the contents back to the file
     file_put_contents($file, $current);
 
