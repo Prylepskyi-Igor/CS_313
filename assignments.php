5f7f3ebf032b3b65ac48ebc4262b764543f96cba
Igor Prylepskyi <pry12002@byui.edu>
2015-05-08 22:09:01 -0600

    commit

--- assignments.php
+++ assignments.php
@@ -25,7 +25,7 @@
         	<table id="assignments">
                 <td>
                     <a href="team-readiness-activity.html">Team Readiness Activity</a><br>
-                    <a href="php-survey.php">Team Readiness Activity</a>
+                    <a href="php-survey.php">PHP Survey</a>
                 </td>
             </table>
         </main>
