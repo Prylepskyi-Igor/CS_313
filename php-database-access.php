a49198decb8ed026e76e50adbc2e662a4c8d1894
Igor Prylepskyi <pry12002@byui.edu>
2015-05-31 01:35:37 -0600

    lol

--- php-database-access.php
+++ php-database-access.php
@@ -12,6 +12,12 @@
         require("dbConnector.php");
 
         $db = loadDatabase();
+
+        if (isset($_GET['delete'])) {
+            $stmt = $db->prepare('DELETE FROM photos WHERE P_ID = :P_ID');
+            $stmt->bindValue(':P_ID', $_SESSION["P_ID"]);
+            $stmt->execute();
+        }
     ?>
     <title>Album</title>
 </head>
