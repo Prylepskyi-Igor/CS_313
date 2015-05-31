dc85c71f6e6db8fc38534ddadbe87f7e8cc83a4d
Igor Prylepskyi <pry12002@byui.edu>
2015-05-31 00:57:51 -0600

    lol

--- display_photo.php
+++ display_photo.php
@@ -40,18 +40,9 @@
         $stmt = $db->prepare('DELETE FROM photos WHERE P_ID = :P_ID');
         $stmt->bindValue(':P_ID', $_SESSION["P_ID"]);
         $stmt->execute();
-
-        //ob_start();
-//
-        //$url =  "display_photo.php";
-//
-        //while (ob_get_status()) 
-        //{
-        //    ob_end_clean();
-        //}
-//
-        //header( "Location: $url" );
         ?>
+
+        location.reload();
     }
     </script>
 
