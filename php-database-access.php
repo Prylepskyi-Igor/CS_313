203fcb1e2f0dba4f2f6f31261c03a3613e721d7b
Igor Prylepskyi <pry12002@byui.edu>
2015-05-31 01:46:51 -0600

    lol

--- php-database-access.php
+++ php-database-access.php
@@ -18,7 +18,7 @@
             $stmt->bindValue(':P_ID', $_SESSION["P_ID"]);
             $stmt->execute();
 
-            $_SESSION["P_ID"] = 0;
+            session_unset();
         }
     ?>
     <title>Album</title>
