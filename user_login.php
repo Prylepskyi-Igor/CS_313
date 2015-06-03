f72cd0c8f4d1c431eccb71480af903e8fe4f1a9a
Igor Prylepskyi <pry12002@byui.edu>
2015-06-02 23:32:20 -0600

    lol

--- user_login.php
+++ user_login.php
@@ -28,7 +28,17 @@
             if (isset($_GET['user_name']) && isset($_GET['user_password'])) {
                 foreach ($db->query('SELECT user_name, user_password, user_id FROM users') as $row)
                 {
-                    if (password_verify($_GET['user_password'], $row['user_password'])) {
+                    $hash = '$2y$07$BCryptRequires22Chrcte/VlQH0piJtjXl.0t1XkA8pw9dMXTpOq';
+
+                    if (password_verify('rasmuslerdorf', $hash)) {
+                        echo 'Password is valid!';
+                        goto exit_db_loop;
+                    } else {
+                        echo 'Invalid password.';
+                        goto exit_db_loop;
+                    }
+
+                    if (password_verify($_GET['user_password'], $row['user_password']) && $row['user_name'] === $_GET['user_name']) {
                         $_SESSION["user_id"] = $_GET['user_id'];
 
                         ob_start(); 
