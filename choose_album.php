202f99c163c48e1b6d9e0b3b51bfe5b99cdc7cac
Igor Prylepskyi <pry12002@byui.edu>
2015-06-06 13:21:08 -0600

    lol

--- choose_album.php
+++ choose_album.php
@@ -46,11 +46,11 @@
                 $stmt->closeCursor();
 
                 // copy A_ID from albums table to users table
-                $stmt = $db->prepare('UPDATE users SET A_ID = 2 WHERE user_id = 1');
-                $stmt->bindValue(':album_name', $_GET['album_name']);
-                $stmt->bindValue(':newId', $_SESSION['user_id']);
-                $stmt->execute();
-                $stmt->closeCursor();
+                $stm = $db->query('UPDATE users SET A_ID = 2 WHERE user_id = 1');
+                $stm->bindValue(':album_name', $_GET['album_name']);
+                $stm->bindValue(':newId', $_SESSION['user_id']);
+                $stm->execute();
+                $stm->closeCursor();
 
                 // extract last album id from the database
                 //$stmt = $db->prepare("SELECT MAX(A_ID) FROM albums");
