e9ca2e605942694df1709e6527ba781bc0624cdc
Igor Prylepskyi <pry12002@byui.edu>
2015-06-07 00:30:09 -0600

    lol

--- php-database-access.php
+++ php-database-access.php
@@ -17,8 +17,18 @@
             $stmt = $db->prepare('DELETE FROM photos WHERE P_ID = :P_ID');
             $stmt->bindValue(':P_ID', $_SESSION["P_ID"]);
             $stmt->execute();
+            $stmt->closeCursor();
 
-            session_unset();
+            ob_start(); 
+
+            $url =  "php-database-access.php";
+
+            while (ob_get_status()) 
+            {
+                ob_end_clean();
+            }
+                
+            header( "Location: $url" );
         }
 
         if (isset($_GET['photo_path'])) {
